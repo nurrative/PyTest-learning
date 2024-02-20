1c07af4a996860c795119724bd1c2eccffd39d4d
nurrative <nursul1997@gmail.com>
2024-02-20 14:20:58 +0600

    added calculator pytest and raises

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
-class A:
-    x=1
+from typing import Union
+
+
+class Calculator:
+    def divide(self, x: Union[int, float], y: Union[int, float])-> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments should be numeric")
+        if y==0:
+            raise ZeroDivisionError("Cannot divide by Zero")
+        return x / y
+    
+    def add(self, x: Union[int, float], y: Union[int, float])-> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments should be numeric")
+        return x + y
+    
+
+if '__name__' == '__main__':
+    calculator = Calculator()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,32 @@
-from src.main import A
-def test_main():
-    assert A.x==1
+import pytest
+from contextlib import nullcontext as does_not_raise
+from src.main import Calculator
 
-def test_2():
-    assert 2==2
+
+class TestCalculator:
+    @pytest.mark.parametrize(
+            "x, y, res, expectation",
+            [
+                (1, 2, 0.5, does_not_raise()),
+                (5, -1, -5, does_not_raise()),
+                (5, "-1", -5, pytest.raises(TypeError)),
+                (5, 0, -5, pytest.raises(ZeroDivisionError)),
+            ]
+    )
+    def test_divide(self, x, y , res, expectation):
+        with expectation:
+            assert Calculator().divide(x, y) == res
+
+
+    @pytest.mark.parametrize(
+            "x, y, res, expectation",
+            [
+                (1, 2, 3, does_not_raise()),
+                (5, -1, 4, does_not_raise()),
+                (5, "-1", 4, pytest.raises(TypeError)),
+            ]
+    )
+    def test_add(self, x, y , res, expectation):
+        with expectation:
+            assert Calculator().add(x, y) == res
+
